5d51c7562b0e04614ecc1df87d58d9b80b97528f
itskpalusa <karthik.palusa@gmail.com>
2021-03-23 18:19:04 -0600

    fuel index includes bootstrap

--- app/views/m1/index.php
+++ app/views/m1/index.php
@@ -9,6 +9,9 @@ the other pages on the site. -->
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="../assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
     <title>GoreTex - Home</title>
 </head>
 
