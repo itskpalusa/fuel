420233a3c6ef8d3002d53ad9dcbcd43fb75bb389
itskpalusa <karthik.palusa@gmail.com>
2021-03-24 19:00:31 -0600

    new index and about files

--- app/views/m1/index.php
+++ app/views/m1/index.php
@@ -6,54 +6,34 @@ the other pages on the site. -->
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="../assets/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-    <title>GoreTex - Home</title>
-</head>
-
 <body>
 
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="color-generator.php">Color Generator</a></li>
-        </ul>
-    </nav>
+  <header>Goretex Professionals LLC<img src='https://i.pinimg.com/originals/ca/57/5b/ca575bc595713822b92880db4219881a.png' alt='logo' width='32'></header>
 
-    <header>Goretex Professionals LLC<img
-            src='https://i.pinimg.com/originals/ca/57/5b/ca575bc595713822b92880db4219881a.png' alt='logo' width='32'>
-    </header>
+  <div id="content">
 
-    <div id="content">
+  <div id=welcome>
+    <h1>Outstanding Designers & Website Developers</h1>
+    <h2>Our team has been crafting beautiful websites since 2021. We create sites that are lightning fast and tailored to your needs.</h2>
+  </div>
 
-        <div id=welcome>
-            <h1>Outstanding Designers & Website Developers</h1>
-            <h2>Our team has been crafting beautiful websites since 2021. We create sites that are lightning fast and
-                tailored to your needs.</h2>
-        </div>
-
-        <div id=contact>
-            <!-- the email doesn't get sent anywhere -->
-            <form action="home.php" method="post">
-                <label for="email">
-                    <h1>Contact us for a free consultation!</h1>
-                </label>
-                <input type="email" id="email" name="email" placeholder="Enter your email">
-                <input type="submit" value="submit">
-            </form>
-        </div>
-        <?php
+    <div id=contact>
+      <!-- the email doesn't get sent anywhere -->
+      <form action="home.php" method="post">
+        <label for="email">
+          <h1>Contact us for a free consultation!</h1>
+        </label>
+        <input type="email" id="email" name="email" placeholder="Enter your email">
+        <input type="submit" value="submit">
+      </form>
+    </div>
+    <?php
     if (isset($_POST['email'])) {
       echo "Thank you! You'll hear form us shortly.";
     }
     ?>
-    </div>
-    <footer>Copyright "GoreTex Professionals LLC" 2021</footer>
+  </div>
+  <footer>Copyright "GoreTex Professionals LLC" 2021</footer>
 </body>
 
 </html>
